7061638cd4a122826c4d286dc003fe6a83cb124e
justkaneri <justkaneri@yandex.ru>
2026-09-03 19:03:23 +0300

    test: валидация CreateServiceRequest

--- PsySchedule/Validations/ServiceDtoValidator.cs
+++ PsySchedule/Validations/ServiceDtoValidator.cs
@@ -13,8 +13,8 @@ namespace PsySchedule.Validations
                     .WithMessage("Название услуги не должно быть пустым и должно быть короче 100 символов");
 
             RuleFor(s => s.Price)
-                   .GreaterThan(0)
-                   .LessThan(500_000)
+                   .GreaterThan(-1)
+                   .LessThan(500_001)
                    .WithMessage("Цена должна быть в диапазоне от 0 до 500000");
         }
     }
---
+++ PsyScheduleTests/ValidationsTests/ServiceDtoValidatorTest.cs
@@ -0,0 +1,68 @@
+﻿using PsySchedule.Dto;
+using PsySchedule.Validations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PsyScheduleTests.ValidationsTests
+{
+    public class ServiceDtoValidatorTest
+    {
+        private readonly ServiceDtoValidator _validator;
+
+        public ServiceDtoValidatorTest()
+        {
+            _validator = new ServiceDtoValidator();
+        }
+
+        [Fact]
+        public void Correct_Test()
+        {
+            CreateServiceRequest request = new CreateServiceRequest("Консульация", 500);
+
+            var result = _validator.Validate(request);
+
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void Empty_Name_Test()
+        {
+            CreateServiceRequest request = new CreateServiceRequest("", 500);
+
+            var result = _validator.Validate(request);
+
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void Zero_Price_Test()
+        {
+            CreateServiceRequest request = new CreateServiceRequest("Консультация", 0);
+
+            var result = _validator.Validate(request);
+
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void Price_On_Bord_Test()
+        {
+            CreateServiceRequest request = new CreateServiceRequest("Консультация", 500_000);
+
+            var result = _validator.Validate(request);
+
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void Price_Less_Zero_Test()
+        {
+            CreateServiceRequest request = new CreateServiceRequest("Консультация", -1);
+
+            var result = _validator.Validate(request);
+
+            Assert.False(result.IsValid);
+        }
+    }
+}
