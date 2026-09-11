9d3fc2a35f52601162acd9159dbfecfa0fb6b317
justkaneri <justkaneri@yandex.ru>
2026-09-11 19:10:33 +0300

    dev: миграция бд, добавил индекс для отпуска

---
+++ PsySchedule/Migrations/20260911160925_AddVacationIndexs.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PsySchedule.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVacationIndexs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Vacation_StartedAt_FinishedAt",
+                table: "Vacation",
+                columns: new[] { "StartedAt", "FinishedAt" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Vacation_StartedAt_FinishedAt",
+                table: "Vacation");
+        }
+    }
+}
